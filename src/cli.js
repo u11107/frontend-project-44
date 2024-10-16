f97f829d42c9d5e814f2ddbdd126bc14f30e348d
Rinat <eenot84@yandex.ru>
2024-10-16 19:36:35 +0400

    refactor 1

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 export const welcomeUser = () => {
-    console.log('brain-games')
+    console.log('brain-games');
     console.log('Welcome to the Brain Games!');
     const userName = readlineSync.question('What is your name? ');
-    console.log(`Hello, ${userName}`);
-}
+    console.log(`Hello, ${userName}!`);
+};
 
