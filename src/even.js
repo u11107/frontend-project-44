1458dea183ed4e0eec261cb0036e4bee65fc4694
Rinat <eenot84@yandex.ru>
2024-10-16 21:14:46 +0400

    Airbnb

--- src/even.js
+++ src/even.js
@@ -1,13 +1,17 @@
 import { playGame, welcomeUser } from './index.js';
 
 const isEven = (num) => num % 2 === 0;
+
 const gameEven = () => {
   const userName = welcomeUser();
+
   const getQuestionAndAnswer = () => {
     const number = Math.floor(Math.random() * 100);
     const correctAnswer = isEven(number) ? 'yes' : 'no';
     return [number, correctAnswer];
   };
+
   playGame(userName, getQuestionAndAnswer);
 };
-export default gameCalc;
+
+export default gameEven;
