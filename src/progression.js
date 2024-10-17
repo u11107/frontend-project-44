04dc9e1463087d49a0ba73e07b7f80322f65e69a
Rinat <eenot84@yandex.ru>
2024-10-17 11:00:48 +0400

    Airbnb

--- src/progression.js
+++ src/progression.js
@@ -2,7 +2,7 @@ import { playGame, welcomeUser } from './index.js';
 
 const generateProgression = (start, step, length) => {
   const progression = [];
-  for (let i = 0; i < length; i = i + 1) {
+  for (let i = 0; i < length; i += 1) {
     progression.push(start + i * step);
   }
   return progression;
