fa6e4270c3f3ae6c34ea89cb30b3adbc2c18b5cc
Rinat <eenot84@yandex.ru>
2024-10-19 17:07:01 +0400

    refactor cod

--- src/game/even.js
+++ src/game/even.js
@@ -1,15 +1,17 @@
 import { playGame, welcomeUser } from './index.js';
-import {getRandomNumber} from "../utils/utils.js";
+import { getRandomNumber } from "../utils/utils.js";
 
 const isEven = (num) => num % 2 === 0;
 
 const gameEven = () => {
   const userName = welcomeUser();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
   const getQuestionAndAnswer = () => {
-    const number = getRandomNumber(1,100);
+    const number = getRandomNumber(1, 100);
     const correctAnswer = isEven(number) ? 'yes' : 'no';
-    return [number, correctAnswer];
+    const question = `Question: ${number}`;
+    return [question, correctAnswer];
   };
 
   playGame(userName, getQuestionAndAnswer);
