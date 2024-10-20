9847818dc16a0f5b70a81a886e52d20d8535baed
Rinat <eenot84@yandex.ru>
2024-10-20 13:23:34 +0400

    refactor gcd to the standar eslint

--- src/game/gcd.js
+++ src/game/gcd.js
@@ -1,7 +1,10 @@
 import { playGame } from './index.js';
-import {getRandomNumber} from "../utils/utils.js";
+import { getRandomNumber } from '../utils/utils.js';
 
-const getGcd = (a, b) => {
+const getGcd = (num1, num2) => {
+  let a = num1;
+  let b = num2;
+
   while (b !== 0) {
     const temp = b;
     b = a % b;
@@ -11,8 +14,8 @@ const getGcd = (a, b) => {
 };
 
 const generateQuestionAndAnswer = () => {
-  const num1 = getRandomNumber(1,100);
-  const num2 = getRandomNumber(1,100);
+  const num1 = getRandomNumber(1, 100);
+  const num2 = getRandomNumber(1, 100);
   const question = `${num1} ${num2}`;
   const correctAnswer = String(getGcd(num1, num2));
   return [question, correctAnswer];
