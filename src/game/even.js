deb5a1e7dac0a56b4a7dd213817bb7c8b56ce2a7
Rinat <eenot84@yandex.ru>
2024-10-20 13:13:15 +0400

    refactor evenGame

--- src/game/even.js
+++ src/game/even.js
@@ -1,18 +1,17 @@
-import { playGame, welcomeUser } from './index.js';
-import { getRandomNumber } from "../utils/utils.js";
+import { playGame } from './index.js';
+import { getRandomNumber } from '../utils/utils.js';
 
 const isEven = (num) => num % 2 === 0;
 
-const gameEven = () => {
-  const userName = welcomeUser();
+const getQuestionAndAnswer = () => {
+  const number = getRandomNumber(1, 100);
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  return [String(number), correctAnswer];
+};
 
-  const getQuestionAndAnswer = () => {
-    const number = getRandomNumber(1, 100);
-    const correctAnswer = isEven(number) ? 'yes' : 'no';
-    const question = `${number}`;
-    return [question, correctAnswer];
-  };
-  playGame(userName, getQuestionAndAnswer);
+const gameEven = () => {
+  const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+  playGame(gameDescription, getQuestionAndAnswer);
 };
 
 export default gameEven;
