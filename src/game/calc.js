75ecbc9a529a53979c2a48e27299757f2a1ac87b
Rinat <eenot84@yandex.ru>
2024-10-23 22:47:38 +0400

    refacor

--- src/game/calc.js
+++ src/game/calc.js
@@ -3,14 +3,14 @@ import getRandomNumber from '../utils/utils.js';
 
 const calculate = (num1, num2, operation) => {
   switch (operation) {
-  case '+':
-    return num1 + num2;
-  case '-':
-    return num1 - num2;
-  case '*':
-    return num1 * num2;
-  default:
-    throw new Error(`Unknown operation: '${operation}'`);
+    case '+':
+      return num1 + num2;
+    case '-':
+      return num1 - num2;
+    case '*':
+      return num1 * num2;
+    default:
+      throw new Error(`Unknown operation: '${operation}'`);
   }
 };
 
